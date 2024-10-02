bc4ac41fbcc19ea0508059af8dfda52e2c9176a5
root <root@DESKTOP-FII83V8>
2024-10-02 17:09:49 +0400

    add bin/brain-gcd.js and src/games/gcd.js

---
+++ src/games/gcd.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+import { numberGenerator } from '../utils.js'
+import run from '../index.js'
+const description = 'Find the greatest common divisor of given numbers.'
+
+var gcd = function (a, b) {
+	if (!b) {
+		return a
+	}
+
+	return gcd(b, a % b)
+}
+
+const generateRound = () => {
+	const num1 = numberGenerator(1, 20)
+	const num2 = numberGenerator(1, 20)
+	const question = `${num1} ${num2}`
+	const correctAnswer = gcd(num1, num2).toString()
+	return [question, correctAnswer]
+}
+
+export default () => {
+	run(description, generateRound)
+}
