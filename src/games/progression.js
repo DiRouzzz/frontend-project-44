1341ab832962bdae207bd05cf428014896de9f9a
root <root@DESKTOP-FII83V8>
2024-10-02 18:18:02 +0400

    add bin/brain-progression.js and src/games/progression.js

---
+++ src/games/progression.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+import { numberGenerator, getRandomIndex } from '../utils.js'
+import run from '../index.js'
+const description = 'What number is missing in the progression?'
+
+function progression(start, step, length){
+    const progress = [];
+    for(let i = start; progress.length < length; i +=step){
+        progress.push(i);
+    }
+    return progress;
+
+}
+
+const generateRound = () => {
+	const start = numberGenerator(1, 30);
+	const step = numberGenerator(1, 10);
+    const length = numberGenerator(5, 10);
+    const progress = progression(start, step, length) 
+	const indexHidden = getRandomIndex(progress);
+	const correctAnswer = progress[indexHidden].toString()
+    progress[indexHidden] = '..';
+    const question = progress.join(' ');
+	return [question, correctAnswer]
+}
+
+export default () => {
+	run(description, generateRound)
+}
